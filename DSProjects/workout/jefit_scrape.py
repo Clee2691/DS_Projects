0ed4503fbf0f093d3900cdb1cd0bfd40bcb8b952
Calvin <Clee2691@gmail.com>
2020-02-19 18:23:18 -0500

    added looping through years, check if file is already there, added some doc strings

--- DSProjects/workout/jefit_scrape.py
+++ DSProjects/workout/jefit_scrape.py
@@ -2,8 +2,17 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 from datetime import datetime
+from pathlib import Path
+
 
 def date_list_maker(year):
+    """
+    Makes a list of each day given the year
+
+    arugment: Year
+
+    Returns: list of days in YYYY-MM-DD
+    """
     
     date_list = []
 
@@ -11,12 +20,20 @@ def date_list_maker(year):
         for day in range(1, 32):
             try:
                 date_list.append(datetime(year, month, day).strftime("%Y-%m-%d"))
-                #print(datetime(2020, month, day).strftime("%Y-%m-%d"))
-            except ValueError:
+            except ValueError: # If day is out of range, skip it ie. no 30th of february
                 pass
+
     return date_list
 
+
 def entry_grabber(exercise_elements):
+    """
+    Return list of exercises from page
+
+    Arguments: exercise_elements - html block corresponding the exercise
+
+    return: list of exercises for specified day
+    """
     log_entry_list = []
 
     for exercise in exercise_elements:
@@ -40,15 +57,53 @@ def entry_grabber(exercise_elements):
 
     return log_entry_list
 
+
 def csv_maker(entry):
-    try:
-        with open('workout_list.csv', 'a', newline='') as workout_file:
-            writer=csv.writer(workout_file, delimiter=',')
-            for data in entry:
+
+    """
+    Write the list of exercises into a csv file
+
+    arguments: list of exercise entries
+
+    outputs a csv file
+    """
+
+    with open('workout_list.csv', 'a', newline='') as workout_file:
+        writer=csv.writer(workout_file, delimiter=',')
+        for data in entry:
             writer.writerow(data)
 
-    except:
-        with open('workout_list.csv', 'w', newline ='') as workout_file:
-            writer = csv.writer(workout_file, delimiter=',')
-            writer.writerow(['date', 'log_id', 'exercise_id', 'exercise_name', 'set_number', 'weight', 'rep'])
 
+if __name__ == '__main__':
+
+    base_url = 'https://www.jefit.com/members/user-logs/log/?xid='
+    user_id = '3806546'
+
+    #specific_date = '2020-02-05' # Must be in YYYY-MM-DD
+    
+    for year in [2016, 2017, 2018, 2019, 2020]:
+
+        date_list = date_list_maker(year) # Make a list of dates for the year specified
+
+        for date in date_list: 
+            request_url = base_url + user_id + '&dd=' + date # Make the url with specified date
+
+            user_workout = requests.get(request_url).text # Grab HTML
+            workout_html_parse = BeautifulSoup(user_workout) # Parse HTML
+
+            exercises_only = workout_html_parse(class_="exercise-block")
+
+            if exercises_only == []: # Empty list means no exercises for that time frame so pass
+                pass
+            else:
+                entry = entry_grabber(exercises_only)
+                
+                if Path('workout_list.csv').is_file(): # Check if file exists
+                    csv_maker(entry)
+
+                else:
+                    with open('workout_list.csv', 'w+', newline ='') as workout_file:
+                        writer = csv.writer(workout_file, delimiter=',')
+                        writer.writerow(['date', 'log_id', 'exercise_id', 'exercise_name', 'set_number', 'weight', 'rep'])
+
+                    csv_maker(entry)
