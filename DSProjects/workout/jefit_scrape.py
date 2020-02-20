d496a7fd1ad2d74c91c6271e80c2e5554c21e0e9
Calvin <Clee2691@gmail.com>
2020-02-19 20:38:51 -0500

    updated gitignore, formatted jefit_scrape.py with black

--- DSProjects/workout/jefit_scrape.py
+++ DSProjects/workout/jefit_scrape.py
@@ -13,14 +13,14 @@ def date_list_maker(year):
 
     Returns: list of days in YYYY-MM-DD
     """
-    
+
     date_list = []
 
     for month in range(1, 13):
         for day in range(1, 32):
             try:
                 date_list.append(datetime(year, month, day).strftime("%Y-%m-%d"))
-            except ValueError: # If day is out of range, skip it ie. no 30th of february
+            except ValueError:  # If day is out of range, skip it ie. no 30th of february
                 pass
 
     return date_list
@@ -38,20 +38,33 @@ def entry_grabber(exercise_elements):
 
     for exercise in exercise_elements:
         # Exercise Details
-        log_id = exercise.select("input[name='logid']")[0]['value']
-        exercise_id = exercise.select("input[name='exerciseid']")[0]['value']
-        exercise_name = exercise.select("input[name='ename']")[0]['value']
-        log_date = exercise.select("input[name='date']")[0]['value']
+        log_id = exercise.select("input[name='logid']")[0]["value"]
+        exercise_id = exercise.select("input[name='exerciseid']")[0]["value"]
+        exercise_name = exercise.select("input[name='ename']")[0]["value"]
+        log_date = exercise.select("input[name='date']")[0]["value"]
 
         x = 1
         # weight and rep for each exercise
-        for weight, rep in zip(exercise.select("input[name='weight']"), exercise.select("input[name='rep']")):
+        for weight, rep in zip(
+            exercise.select("input[name='weight']"),
+            exercise.select("input[name='rep']"),
+        ):
 
-            set_number = x # Specify which set number it is; not hardcoded in a value
-            weight_value = weight['value']
-            rep_value = rep['value']
+            set_number = x  # Specify which set number it is; not hardcoded in a value
+            weight_value = weight["value"]
+            rep_value = rep["value"]
 
-            log_entry_list.append([log_date, log_id, exercise_id, exercise_name, set_number, weight_value, rep_value])
+            log_entry_list.append(
+                [
+                    log_date,
+                    log_id,
+                    exercise_id,
+                    exercise_name,
+                    set_number,
+                    weight_value,
+                    rep_value,
+                ]
+            )
 
             x += 1
 
@@ -68,42 +81,56 @@ def csv_maker(entry):
     outputs a csv file
     """
 
-    with open('workout_list.csv', 'a', newline='') as workout_file:
-        writer=csv.writer(workout_file, delimiter=',')
+    with open("workout_list.csv", "a", newline="") as workout_file:
+        writer = csv.writer(workout_file, delimiter=",")
         for data in entry:
             writer.writerow(data)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    base_url = 'https://www.jefit.com/members/user-logs/log/?xid='
-    user_id = '3806546'
+    base_url = "https://www.jefit.com/members/user-logs/log/?xid="
+    user_id = "3806546"
 
-    #specific_date = '2020-02-05' # Must be in YYYY-MM-DD
-    
+    # specific_date = '2020-02-05' # Must be in YYYY-MM-DD
+
     for year in [2016, 2017, 2018, 2019, 2020]:
 
-        date_list = date_list_maker(year) # Make a list of dates for the year specified
+        date_list = date_list_maker(year)  # Make a list of dates for the year specified
 
-        for date in date_list: 
-            request_url = base_url + user_id + '&dd=' + date # Make the url with specified date
+        for date in date_list:
+            request_url = (
+                base_url + user_id + "&dd=" + date
+            )  # Make the url with specified date
 
-            user_workout = requests.get(request_url).text # Grab HTML
-            workout_html_parse = BeautifulSoup(user_workout) # Parse HTML
+            user_workout = requests.get(request_url).text  # Grab HTML
+            workout_html_parse = BeautifulSoup(user_workout)  # Parse HTML
 
             exercises_only = workout_html_parse(class_="exercise-block")
 
-            if exercises_only == []: # Empty list means no exercises for that time frame so pass
+            if (
+                exercises_only == []
+            ):  # Empty list means no exercises for that time frame so pass
                 pass
             else:
                 entry = entry_grabber(exercises_only)
-                
-                if Path('workout_list.csv').is_file(): # Check if file exists
+
+                if Path("workout_list.csv").is_file():  # Check if file exists
                     csv_maker(entry)
 
                 else:
-                    with open('workout_list.csv', 'w+', newline ='') as workout_file:
-                        writer = csv.writer(workout_file, delimiter=',')
-                        writer.writerow(['date', 'log_id', 'exercise_id', 'exercise_name', 'set_number', 'weight', 'rep'])
+                    with open("workout_list.csv", "w+", newline="") as workout_file:
+                        writer = csv.writer(workout_file, delimiter=",")
+                        writer.writerow(
+                            [
+                                "date",
+                                "log_id",
+                                "exercise_id",
+                                "exercise_name",
+                                "set_number",
+                                "weight",
+                                "rep",
+                            ]
+                        )
 
                     csv_maker(entry)
